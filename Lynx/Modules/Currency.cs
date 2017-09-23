41695281accaf250962e54ad3f6ad2a58d442b7d
Unknown <vl.ado.thebastard@gmail.com>
2017-09-23 18:24:36 +0200

    add curency

---
+++ Lynx/Modules/Currency.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Lynx.Modules
+{
+    public class Currency : ModuleBase
+    {
+        static Random RNG = new Random();
+    }
+}
