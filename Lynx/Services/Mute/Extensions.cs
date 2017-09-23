ce00d33c47267bf33548b5046a1ed4a5827c0762
Unknown <vl.ado.thebastard@gmail.com>
2017-09-23 02:16:58 +0200

    remove RemoveUser

--- Lynx/Services/Mute/Extensions.cs
+++ Lynx/Services/Mute/Extensions.cs
@@ -11,31 +11,6 @@ namespace Lynx.Services.Mute
 {
     public static class Extensions
     {
-        static Timer Timer;
-        public static void RemoveUser(DiscordSocketClient Client)
-        {
-            Timer = new Timer(_ =>
-            {
-                var Config = Client.LoadMuteList();
-                var Mutelist = Config.MuteList;
-                Task.WhenAll(Mutelist.Select(async snc =>
-                {
-                    if (DateTime.Now > snc.Value.UnmuteTime)
-                    {
-                        var Guild = Client.GetGuild(Convert.ToUInt64(snc.Value.GuildId));
-                        var User = Client.GetGuild(Convert.ToUInt64(snc.Value.GuildId)).GetUser(Convert.ToUInt64(snc.Key)) as SocketGuildUser;
-                        await (User as SocketGuildUser).RemoveRoleAsync((Guild.GetRole(Convert.ToUInt64(Guild.LoadServerConfig().Moderation.MuteRoleID))));
-                        await User.Guild.GetLogChannel().SendMessageAsync($"", embed: new EmbedBuilder().WithSuccesColor().WithDescription($"**{User}** has been **unmuted** from text and voice chat.").WithFooter(x =>
-                        {
-                            x.Text = $"{User} | [Automatic Message]";
-                            x.IconUrl = User.GetAvatarUrl();
-                        }).Build());
-                        await User.GetOrCreateDMChannelAsync().Result.SendMessageAsync("");
-                        await Client.UpdateMuteList(User, null, UpdateHandler.MuteOption.Unmute);
-                    }
-                }));
-            }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
-        }
         public static DateTime GetTime(int Value, string Time)
         {
             var DT = DateTime.Now;
